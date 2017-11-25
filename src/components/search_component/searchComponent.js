fe5d03d193281088f9aa3b3b56031d02aee1ee1f
ELUHIMGADOL <ELUHIMGADOL@users.noreply.github.com>
2017-11-25 03:16:00 +0200

    :syringe: add google Autocomplete & refine :art:

--- src/components/search_component/searchComponent.js
+++ src/components/search_component/searchComponent.js
@@ -13,11 +13,15 @@ export default {
         },
         initLoader: {
             type: Boolean
+        },
+        placeName: {
+            type: String
         }
     },
     data() {
         return {
             query: '',
+            vali: {'good':false,'ok':false,'bad':false},
             err: {'msg':'','val': false},
             loading: false,
         }
@@ -28,6 +32,12 @@ export default {
                 rat     = /[^a-zA-Z ]/gi,
                 q       = this.query.match(pat),
                 Nt_V    = rat.test(this.query);
+
+                // if autocomplete name chosen put it in place of the query..
+                if(this.placeName != '') this.query = this.placeName;
+
+                // hide autocomplete menu , no need to be still opened
+                document.querySelector('.pac-container') ? document.querySelector('.pac-container').style.display = 'none' : '';
 
             if (this.query != '' && q != null && this.query.length > 1 && Nt_V != true) {
                 this.err.msg = '';
@@ -43,5 +53,36 @@ export default {
         },
 
     },
+    computed: {
+        sign: function () {
+            return () => {
+                let el = document.querySelector('#search-component .query');
+
+                if (this.query.length == 0) {
+                    el.querySelector('span').style.width = 0+'%';
+                    this.vali.good  = false;
+                    this.vali.ok    = false;
+                    this.vali.bad   = false;
+                }
+
+                if (this.query.match(/^[a-zA-Z]/gi) && this.query.length == 1) {
+                    el.querySelector('span').style.width = 50+'%';
+                    this.vali.good  = false;
+                    this.vali.ok    = true;
+                    this.vali.bad   = false;
+                }else if(this.query.match(/^[a-zA-Z]/gi) && this.query.length > 1) {
+                    el.querySelector('span').style.width = 100+'%';
+                    this.vali.good  = true;
+                    this.vali.ok    = false;
+                    this.vali.bad   = false;
+                }else if (!this.query.match(/^[a-zA-Z]/gi) && this.query.length > 0) {
+                    el.querySelector('span').style.width = 100+'%';
+                    this.vali.good  = false;
+                    this.vali.ok    = false;
+                    this.vali.bad   = true;
+                }
+            }
+        }
+    }
 
 }
